8037f3542d09cdec04fe09cc01ddd2070fe0a843
Rockon <rockonthegamer@gmail.com>
2024-12-04 02:05:13 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import './App.css'
 import Timer from './components/Timer';
 import Settings from './components/Settings';
 import {useState} from "react";
-
+import SettingsContext from "./components/SettingsContext";
 
 function App() {
 
   const [showSettings, setShowSettings] = useState(true);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMiuntes] = useState(15);
 
   return (
     <>
      <main>
-        {showSettings ? <Settings /> : <Timer />}
+        <SettingsContext.Provider value={{
+            workMinutes,
+            breakMinutes,
+
+            setWorkMinutes, 
+            setBreakMiuntes,
+
+        }}>
+        {showSettings ? <Settings /> : <Timer />} 
+        </SettingsContext.Provider>
         {/* <Settings />
         <Timer /> */}
      </main>
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,26 +1,32 @@
 import ReactSlider from "react-slider";
 import "./slider.css";
+import { useContext } from "react";
+import SettingsContext from "./SettingsContext";
 
 function Settings() {
+
+  const settingsInfo = useContext(SettingsContext);
   return (
     <div style={{ textAlign: "left" }}>
       <h2>Settings:</h2>
-      <label>Work Minutes:</label>
+      <label>Work Minutes: {settingsInfo.workMinutes}:00</label>
       <ReactSlider
         className={"slider"}
         thumbClassName={"thumb"}
         trackClassName={"track"}
-        value={45}
+        value={settingsInfo.workMinutes}
+        onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
         min={1}
         max={120}
       />
-      <label>Break Minutes:</label>
-      <label>Work Minutes:</label>
+      <label>Break Minutes: {settingsInfo.breakMinutes}:00</label>
+      
       <ReactSlider
         className={"slider-green"}
         thumbClassName={"thumb-green"}
         trackClassName={"track"}
-        value={45}
+        value={settingsInfo.breakMinutes}
+        onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
         min={1}
         max={120}
       />
